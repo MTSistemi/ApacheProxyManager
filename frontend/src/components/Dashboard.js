ee9073984ed93a7ee4ad59b9cb2e67c11d0cc13b
Mattia Tadini <61477413+MTSistemi@users.noreply.github.com>
2024-11-29 00:31:44 +0100

    Create Dashboard.js

---
+++ frontend/src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard">
+      <h1>Dashboard</h1>
+      {/* Qui puoi aggiungere i dettagli del dashboard */}
+      <p>BENVENUTO NELLA DASHBOARD!</p>
+    </div>
+  );
+};
+
+export default Dashboard;
